f4b60680a164da8f06b17ea5e0a47fe5d7f4ff16
Erica Basak <erica.basak@ibm.com>
2023-10-13 13:43:42 -0400

    feat(tutorial): complete step 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './app.scss';
-import { Content, Theme } from '@carbon/react';
-import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Theme, Content } from '@carbon/react';
+import TutorialHeader from './components/TutorialHeader';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
+import './app.scss';
 
 class App extends Component {
   render() {
